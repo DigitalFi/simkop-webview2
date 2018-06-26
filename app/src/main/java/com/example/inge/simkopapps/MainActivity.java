3e4c0bc97a0e8cd2df3a1a6734ea2f0f98bae76a
Inge Arfatikka <ingearfatikka@gmail.com>
2018-06-26 13:57:47 +0700

    first commit

---
+++ app/src/main/java/com/example/inge/simkopapps/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.inge.simkopapps;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Menu_Simkop.class);
+                startActivity(intent);
+            }
+        }, SPLASH_SCREEN);
+    }
+}
