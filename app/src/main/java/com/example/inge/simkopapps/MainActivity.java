d513b3efb5327e28dfa770fcb183b1b6f279b4c1
Inge Arfatikka <ingearfatikka@gmail.com>
2018-06-27 15:36:43 +0700

    Grid Menu
    Ganti Icon, Ganti warna, Tambah Icon dan Tambah Splashcreen + Benerin Force Close

--- app/src/main/java/com/example/inge/simkopapps/MainActivity.java
+++ app/src/main/java/com/example/inge/simkopapps/MainActivity.java
@@ -4,22 +4,40 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
+    private static int splashInterval = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(new Runnable() {
+
+
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Menu_Simkop.class);
-                startActivity(intent);
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, Menu_Simkop.class);
+                startActivity(i); // menghubungkan activity splashscren ke main activity dengan intent
+
+
+                //jeda selesai Splashscreen
+                this.finish();
             }
-        }, SPLASH_SCREEN);
-    }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
 }
--- app/src/main/java/com/example/inge/simkopapps/Menu_Simkop.java
+++ app/src/main/java/com/example/inge/simkopapps/Menu_Simkop.java
@@ -1,7 +1,9 @@
 package com.example.inge.simkopapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu_Simkop extends AppCompatActivity {
 
@@ -10,4 +12,35 @@ public class Menu_Simkop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_simkop);
     }
+
+    public void Jumlah_Koperasi(View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Jumlah_Koperasi.class);
+        startActivity(intent);
+    }
+
+    public void Jumlah_Anggota(View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Jumlah_Anggota.class);
+        startActivity(intent);
+    }
+
+    public void Jumlah_Manager(View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Jumlah_Manager.class);
+        startActivity(intent);
+    }
+
+    public void Jumlah_Asset(View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Jumlah_Asset.class);
+        startActivity(intent);
+    }
+
+    public void Jumlah_Omset (View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Jumlah_Omset.class);
+        startActivity(intent);
+    }
+
+    public void Data_Koperasi(View view) {
+        Intent intent = new Intent(Menu_Simkop.this, Data_Koperasi.class);
+        startActivity(intent);
+    }
 }
+
